69621939d06a2760ba46691a8b2796aa47797e04
Renata Pulz <renata.pulz@raizen.com>
2024-07-01 08:54:43 -0300

    Finalizado criação da estrutura da estrutura do projeto, estabelecido conexão com o banco e desenvolvido rota a create em Clients.

--- src/routes/clients.routes.js
+++ src/routes/clients.routes.js
@@ -1,5 +1,10 @@
 const {Router} = require('express')
-    const clientsRoutes = new Router()
+const ClientController = require ('../controllers/ClientController')
+
+const clientsRoutes = new Router()
+const clientController = new ClientController
+
+clientsRoutes.post('/', (req, res) => clientController.create(req, res));
 
 
 module.exports = clientsRoutes
